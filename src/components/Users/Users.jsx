b7d406cf57041a0db2d0c24e9434aafb81ae5bd6
Rumata6642 <88488344+Rumata6642@users.noreply.github.com>
2022-05-24 10:32:38 +0300

    Update Users.jsx

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,9 +5,9 @@ import classes from './Users.module.css'
 
 class Users extends React.Component {
 
-  constructor(props) {
-    super(props)
-    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+  componentDidMount() {
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+      .then(response => {
         this.props.setUsers(response.data.items)
       })
   } 
